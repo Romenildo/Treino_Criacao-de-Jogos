1aaf73db3eb0862c6592ec493c37eb6c9753ee1d
꧁ Romenildo꧂ <53869976+Romenildo@users.noreply.github.com>
2020-06-16 21:53:32 -0300

    Create Tiro.java

---
+++ Primeiro # semnome/scr/jogo/modelo/Tiro.java
@@ -0,0 +1,73 @@
+package shottingStar.modelo;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Tiro {
+	
+	private Image imagem;
+	private int x,y; //posicao do tiro
+	private int largura , altura;//largura e altura do tiro
+	private boolean isVisivel; //quando ele chegar no final da tela ele sumir
+	
+	private static final int LARGURA = 938; //posicao maxima da tela//onde os tiro irao sumir
+	private static int VELOCIDADE = 5; //velocidade do tiro
+	
+	public Tiro(int x, int y) {//pega a posicao do player de onde ira sair o tiro
+		this.x = x; //this do tiro vai ser igual ao x do jogador;
+		this.y = y;
+		isVisivel = true;
+	}
+	
+	public void load() {
+		ImageIcon referencia = new ImageIcon("res\\tiro.png");//imagem do tiro
+		imagem = referencia.getImage();
+		
+		this.largura = imagem.getWidth(null);//tamanho de colisao da bala
+		this.altura = imagem.getHeight(null);
+		
+		
+	}
+	
+	public void update() {
+		this.x += VELOCIDADE;
+			if(this.x > LARGURA)
+				isVisivel = false;//caso o tiro passe a largura que é a posicao final da tela ele sumira
+	}
+	
+	
+
+	public boolean isVisivel() {
+		return isVisivel;
+	}
+
+	public void setVisivel(boolean isVisivel) {
+		this.isVisivel = isVisivel;
+	}
+
+	public static int getVELOCIDADE() {
+		return VELOCIDADE;
+	}
+
+	public static void setVELOCIDADE(int vELOCIDADE) {
+		VELOCIDADE = vELOCIDADE;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public Image getImagem() {
+		return imagem;
+	}
+	
+	
+
+	
+	
+}
